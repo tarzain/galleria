02cf9f2c970fe7f4013f381c362257c0e46a6a49
Zain Shah <zain@claralabs.com>
2016-10-17 17:18:49 -0700

    consolidated javascript

--- main.js
+++ main.js
@@ -20,10 +20,67 @@ function callAjax(url, params, callback){
 }
 
 var searchGiphy = function(search_tag, n_images, callback){
-  // this function will return n thumbnail urls for a given search tag query
   var BASE_URL = 'http://api.giphy.com/v1/gifs/search?';
   var params = {'q' : search_tag, 'api_key' : API_KEY, 'limit' : n_images};
 
   callAjax(BASE_URL, params, callback);
 }
 
+var resultsCallback = function(data){
+    results = JSON.parse(data)['data'];
+    gallery = document.getElementById('gallery');
+
+    for(var i = 0; i<=results.length-1; i++){
+        galleryItem = document.getElementById('gallery').children[0];
+        if(i > 0){
+            galleryItem = galleryItem.cloneNode(true);
+            gallery.appendChild(galleryItem);
+        }
+
+        img_url = results[i]['images']['fixed_height_downsampled']['url']
+        img_title = results[i]['slug'].split('-').slice(0,-1).join(' ');
+
+        galleryItem.setAttribute('data-id', i);
+
+        image = galleryItem.children[0].getElementsByTagName('img')[0];
+
+        image.setAttribute('src', img_url);
+        image.setAttribute('title', img_title)
+    }
+}
+
+var toggleLightbox = function(event){
+    document.getElementById('lightbox').classList.toggle('show');
+
+    if(event.className === 'item'){
+        imageId = event.getAttribute('data-id');
+        renderImage(imageId);
+    }
+
+}
+
+var renderImage = function(imageId){
+    imageUrl = results[imageId]['images']['original']['url'];
+    lightboxImage = document.getElementById('bigImage').getElementsByTagName('img')[0];
+    lightboxImage.setAttribute('src', imageUrl);
+}
+
+var checkKey = function(e) {
+    e = e || window.event;
+
+    if (e.keyCode === 37) {
+        if(imageId > 0){
+            imageId--;
+            renderImage(imageId);
+        }
+    }
+    else if (e.keyCode === 39) {
+        if(imageId < results.length-1){
+            imageId++;
+            renderImage(imageId);
+        }
+    }
+    else if (e.keyCode === 27) {
+        toggleLightbox(e);
+    }
+}
